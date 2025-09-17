f40935a7512891e71b0e41de8e455891d1d5bf5c
norbertfrancis <norbertfrancis07@gmail.com>
2025-09-17 15:32:43 +0530

    create a sample  account profile UI

--- client/src/pages/shopping-view/account.jsx
+++ client/src/pages/shopping-view/account.jsx
@@ -1,7 +1,32 @@
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import accImage from "../../assets/bg chat.jpg";
+
 function ShoppingAccount() {
-    return ( 
-        <div>Shopping account page</div>
-     );
+  return (
+    <div className="flex flex-col">
+      <div className="relative h-[250px] w-full overflow-hidden">
+        <img
+          src={accImage}
+          className="h-full w-full object-cover object-center"
+        />
+      </div>
+      <div className="container mx-auto grid grid-cols-1 gap-8 py-8">
+        <div className="flex flex-col rounded-lg border bg-background p-6 shadow-sm">
+            <Tabs defaultValue="orders">
+                <TabsList>
+                    <TabsTrigger value="orders"> Orders</TabsTrigger>
+                    <TabsTrigger value="address">Address</TabsTrigger>    
+                    
+                </TabsList>
+                <TabsContent value="orders">orders Content</TabsContent>
+                <TabsContent value="address">address content</TabsContent>
+
+            </Tabs>
+        </div>
+
+      </div>
+    </div>
+  );
 }
 
 export default ShoppingAccount;
