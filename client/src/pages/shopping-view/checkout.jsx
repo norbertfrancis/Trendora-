849bb4a65e18e2e8ea4d19a686450f73fa0dea9a
norbertfrancis <norbertfrancis07@gmail.com>
2025-09-18 12:37:15 +0530

    finish the Checkout page UI

--- client/src/pages/shopping-view/checkout.jsx
+++ client/src/pages/shopping-view/checkout.jsx
@@ -2,13 +2,27 @@ import { useSelector } from "react-redux";
 import img from "../../assets/bg chat.jpg";
 import Address from "./address";
 import UserCartItemsContent from "@/components/shopping-view/cart-items-content";
+import { Button } from "@/components/ui/button";
 
 function ShoppingCheckout() {
   const { cartItems } = useSelector((state) => state.shopCart);
 
+  const totalCartAmount =
+    cartItems && cartItems.items && cartItems.items.length > 0
+      ? cartItems.items.reduce(
+          (sum, currentItem) =>
+            sum +
+            (currentItem?.salePrice > 0
+              ? currentItem?.salePrice
+              : currentItem?.price) *
+              currentItem?.quantity,
+          0
+        )
+      : 0;
+
   return (
     <div className="flex flex-col">
-      <div className="relative h-[300px] w-full overflow-hidden">
+      <div className="relative h-[200px] w-full overflow-hidden">
         <img src={img} className="h-full w-full object-cover object-center" />
       </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 gap-5 mt-5 p-5">
@@ -19,6 +33,15 @@ function ShoppingCheckout() {
                 <UserCartItemsContent cartItem={item} />
               ))
             : null}
+          <div className="mt-8 space-y-4">
+            <div className="flex justify-between">
+              <span className="font-bold">Total</span>
+              <span className="font-bold">Rs {totalCartAmount}</span>
+            </div>
+          </div>
+          <div className="mt-4 w-full ">
+            <Button className="w-full">Checkout with Paypal</Button>
+          </div>
         </div>
       </div>
     </div>
