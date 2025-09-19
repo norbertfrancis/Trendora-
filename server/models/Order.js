10ebcb55ca3563ff0bfdc3e2327517514c6daee6
norbertfrancis <norbertfrancis07@gmail.com>
2025-09-19 10:26:27 +0530

    create model for Orders for payment

---
+++ server/models/Order.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const OrderSchema = new mongoose.Schema({
+  userId: String,
+  cartItems: [
+    {
+      productId: String,
+      title: String,
+      image: String,
+      price: String,
+      salePrice: String,
+      quantity : Number
+    },
+  ],
+  addressInfo: {
+    addressId: String,
+    address: String,
+    city: String,
+    landmark: String,
+    pincode: String,
+    phone: String,
+    notes: String,
+  },
+  orderStatus : String,
+  paymentMethod : String,
+  paymentStatus : String,
+  totalAmount : Number,
+  orderDate : Date,
+  orderUpdateDate : Date,
+  paymentId : String,
+  payerId : String,
+
+});
+
+module.exports = mongoose.model('Order', OrderSchema)
