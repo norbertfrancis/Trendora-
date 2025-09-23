0cdf8c1d02be857c9b9f06804519f8b7d62a14d8
norbertfrancis <norbertfrancis07@gmail.com>
2025-09-23 11:13:05 +0530

    create asyncThunk for admin view orders

--- client/src/store/admin/order-slice/order-slice.js
+++ client/src/store/admin/order-slice/order-slice.js
@@ -1,18 +1,55 @@
  import { createAsyncThunk, createSlice } from "@reduxjs/toolkit"
+import axios from "axios";
 
 const initialState = {
     orderList: [],
     orderDetails : null,
 }
 
+ export  const getALlOrdersForAdmin = createAsyncThunk(
+    "/order/getAllOrdersForAdmin",
+    async () => {
+        const response = await axios.get(
+            `http://localhost:5000/api/admin/orders/get`
+        );
+        return response.data;
+    }
+ )
+
+ export const getOrderDetailsForAdmin = createAsyncThunk(
+    "/order/getOrderDetailsForAdmin",
+    async (id) => {
+        const response = await axios.get(
+            `http://localhost:5000/api/admin/orders/details/${id}`
+        )
+        return response.data
+    }
+ )
+
  const adminOrderSlice = createSlice({
     name : 'adminOrderSlice' ,
     initialState ,
     reducers : {},
-    extraReducers: (builder)=> {},
+    extraReducers: (builder)=> {
+        builder.addCase(getALlOrdersForAdmin.pending, (state)=> {
+            state.isLoading = true
+        }).addCase(getALlOrdersForAdmin.fulfilled, (state, action) => {
+            state.isLoading = false;
+            state.orderList = action.payload.data
+        }).addCase(getALlOrdersForAdmin.rejected, (state)=> {
+            state.isLoading = false;
+            state.orderList = []
+        }).addCase(getOrderDetailsForAdmin.pending, (state)=> {
+            state.isLoading = true
+        }).addCase(getOrderDetailsForAdmin.fulfilled, (state,action) => {
+            state.isLoading = false;
+            state.orderDetails = action.payload.data;
+        }).addCase(getOrderDetailsForAdmin.rejected, (state)=> {
+            state.isLoading = false;
+            state.orderDetails = null
+        })
+    },
  });
- export  const getALlOrdersForAdmin = createAsyncThunk(
-    "/order/getAllOrdersForAdmin",
- )
+
 
  export default adminOrderSlice.reducer
