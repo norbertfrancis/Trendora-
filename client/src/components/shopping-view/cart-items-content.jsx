43af32b75a46ecfc4afa95b1a0b26635e262b927
norbertfrancis <norbertfrancis07@gmail.com>
2025-09-25 17:31:43 +0530

    set the max limit upto the current quantity of the each product

--- client/src/components/shopping-view/cart-items-content.jsx
+++ client/src/components/shopping-view/cart-items-content.jsx
@@ -9,39 +9,38 @@ import { useToast } from "@/hooks/use-toast";
 
 function UserCartItemsContent({ cartItem }) {
   const { user } = useSelector((state) => state.auth);
-  const {cartItems} = useSelector((state) => state.shopCart);
-  const  {productList} = useSelector(
-    (state) => state.shopProducts
-  )
+  const { cartItems } = useSelector((state) => state.shopCart);
+  const { productList } = useSelector((state) => state.shopProducts);
   const dispatch = useDispatch();
   const { toast } = useToast();
 
   const handleUpdateQuantity = (getCartItem, typeOfAction) => {
+    if (typeOfAction === "plus") {
+      let getCartItems = cartItems.items || [];
 
-    if(typeOfAction === 'plus'){
-          let getCartItems = cartItems.items || [];
+      if (getCartItems.length) {
+        const indexOfCurrentCartItem = getCartItems.findIndex(
+          (item) => item.productId === getCartItem?.productId
+        );
 
-    if (getCartItems.length) {
-      const indexOfCurrentCartItem = getCartItems.findIndex(
-        (item) => item.productId === getCartItem?.productId
-      );
+        const getCurrentProductIndex = productList.findIndex(
+          (product) => product._id === getCartItem?.productId
+        );
+        const getTotalStock = productList[getCurrentProductIndex].totalStock;
 
-      const getCurrentProductIndex = productList.findIndex(product=> product._id === getCartItem?.productId)
-      const getTotalStock = productList[getCurrentProductIndex].totalStock
-
-      if (indexOfCurrentCartItem > -1) {
-        const getQuantity = getCartItems[indexOfCurrentCartItem].quantity;
-        if (getQuantity + 1 > getTotalStock) {
-          toast({
-            title: `Only ${getQuantity} quantity can be added for this item `,
-            variant: "destructive",
-          });
-          return;
+        if (indexOfCurrentCartItem > -1) {
+          const getQuantity = getCartItems[indexOfCurrentCartItem].quantity;
+          if (getQuantity + 1 > getTotalStock) {
+            toast({
+              title: `Only ${getQuantity} quantity can be added for this item `,
+              variant: "destructive",
+            });
+            return;
+          }
         }
       }
     }
-    }
-    
+
     dispatch(
       updateCartQunatity({
         userId: user?.id,
