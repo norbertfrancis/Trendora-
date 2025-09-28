d0b4cb68feb08d8fe2c0457697c39df8e42d9f51
norbertfrancis <norbertfrancis07@gmail.com>
2025-09-28 18:25:26 +0530

    create asyncThunk for review and set in store

--- client/src/store/store.js
+++ client/src/store/store.js
@@ -1,26 +1,28 @@
 import { configureStore } from "@reduxjs/toolkit";
-import authReducer from './auth-slice';
-import adminProductsSlice from './admin/product-slice';
-import adminOrderSlice from './admin/order-slice/order-slice'
-import shopProductsSlice from './shop/product-slice';
-import shopCartSlice from './shop/cart-slice/cart-slice'
-import shopAddressSlice from './shop/address-slice/address-slice'
-import shopOrderSlice from './shop/order-slice'
-import shopSearchSlice from './shop/search-slice/search-slice'
+import authReducer from "./auth-slice";
+import adminProductsSlice from "./admin/product-slice";
+import adminOrderSlice from "./admin/order-slice/order-slice";
+import shopProductsSlice from "./shop/product-slice";
+import shopCartSlice from "./shop/cart-slice/cart-slice";
+import shopAddressSlice from "./shop/address-slice/address-slice";
+import shopOrderSlice from "./shop/order-slice";
+import shopSearchSlice from "./shop/search-slice/search-slice";
+import shopReviewSlice from "./shop/review-slice/review-slice";
 
 const store = configureStore({
-    reducer: {
-        auth: authReducer,
+  reducer: {
+    auth: authReducer,
 
-        adminProducts : adminProductsSlice,
-        adminOrder :adminOrderSlice,
-        
-        shopProducts : shopProductsSlice,
-        shopCart : shopCartSlice,
-        shopAddress: shopAddressSlice,
-        shopOrder : shopOrderSlice,
-        shopSearch : shopSearchSlice,
-    },
-})
+    adminProducts: adminProductsSlice,
+    adminOrder: adminOrderSlice,
+
+    shopProducts: shopProductsSlice,
+    shopCart: shopCartSlice,
+    shopAddress: shopAddressSlice,
+    shopOrder: shopOrderSlice,
+    shopSearch: shopSearchSlice,
+    shopReview: shopReviewSlice,
+  },
+});
 
 export default store;
