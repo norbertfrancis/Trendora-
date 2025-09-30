42e4eabaaf01aee63de5450953c3e0397c3eac0a
norbertfrancis <norbertfrancis07@gmail.com>
2025-09-30 10:31:33 +0530

    set the initial   adminDashboard UI

--- client/src/pages/admin-view/dashboard.jsx
+++ client/src/pages/admin-view/dashboard.jsx
@@ -1,8 +1,23 @@
+import ProductImageUpload from "@/components/admin-view/image-upload";
+import { useState } from "react"
 
 const AdminDashboard = () => {
+  const [imageFile, setImageFile] = useState(null);
+  const [uploadedImageUrl, setUploadedImageUrl] = useState("");
+  const [imageLoadingState, setImageLoadingState] = useState(false)
   return (
     <div>
-       Admin Dashboard
+      <h1>Upload Feature Images</h1>
+      <ProductImageUpload
+        imageFile={imageFile}
+        setImageFile={setImageFile}
+        uploadedImageUrl={uploadedImageUrl}
+        setUploadedImageUrl={setUploadedImageUrl}
+        imageLoadingState={imageLoadingState}
+        setImageLoadingState={setImageLoadingState}
+        // isEditMode={currentEditedId !== null} 
+      />
+
     </div>
   )
 }
