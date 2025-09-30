2dcaf42f02d18133329a53313bdb40b3c6439fc2
norbertfrancis <norbertfrancis07@gmail.com>
2025-09-30 07:57:29 +0530

    reset payment successfull page with view order button

--- client/src/pages/shopping-view/payment-success.jsx
+++ client/src/pages/shopping-view/payment-success.jsx
@@ -1,8 +1,20 @@
+import { Button } from "@/components/ui/button";
+import { Card, CardHeader, CardTitle } from "@/components/ui/card";
+import { useNavigate } from "react-router-dom";
+
 
 
 function PayementSuccessPage() {
+    const navigate = useNavigate()
     return ( 
-        <div>Payement success ............</div>
+        <Card className="p-10">
+            <CardHeader className="p-0">
+                <CardTitle className="text-4xl">
+                   Payment is successfull!
+                </CardTitle>
+            </CardHeader>
+            <Button className="mt-5" onClick={()=> navigate("/shop/account")}>View Orders</Button>
+        </Card>
      );
 }
 
